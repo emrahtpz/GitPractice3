11fbf89f073a628b5462d1d13e096d6bcf52decc
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-18 15:26:43 +0300

    Explain what is hard resetting

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
         System.out.println("Hello world");
         // enhancing hello
         // we are trying to go back in time 1
+        // we are trying to go back in time 2
+        // we are trying to go back in time 3
+        // we are trying to go back in time 4
+        // we are trying to go back in time 5
     }
 }
 
---
+++ src/test/java/ResetPractice.java
@@ -0,0 +1,12 @@
+public class ResetPractice {
+
+    public static void main(String[] args) {
+
+        //work 4 is done
+
+        //removing our branch in time we use reset our branch and we choose hard or mixed restting
+        //which hard rest allow us the go back like nothing happened
+        //mixed is still keeping the work we've done and removing the commits we did
+
+    }
+}
